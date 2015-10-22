14e559c28b8257f94a7ea9bfd65bbaa6074c806f
daniel-anderson-dev <daniel.anderson28562@hotmail.com>
2015-10-22 19:23:16 -0400

    Finished setting up the ViewModels.

--- ContosoUniversity/ViewModels/EnrollmentDateGroup.cs
+++ ContosoUniversity/ViewModels/EnrollmentDateGroup.cs
@@ -5,9 +5,9 @@ namespace ContosoUniversity.ViewModels
 {
 	public class EnrollmentDateGroup
 	{
-		[DataType(DataType.Date]
-		public DateTime? EnrollmentDate {get;set;}
-		
-		public int StudentCount {get;set;}
+		[DataType(DataType.Date)]
+		public DateTime? EnrollmentDate { get; set; }
+
+		public int StudentCount { get; set; }
 	}
 }
