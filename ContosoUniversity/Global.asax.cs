1acbda9b2162593e7c3f506e5361451a10e22cbd
daniel-anderson-dev <daniel.anderson28562@hotmail.com>
2015-10-23 21:01:49 -0400

    * Placed the proper code within Global.asax for the basic site configuration, * Setup the _Layout and _ViewStart items. * Setup the basic Student View.

--- ContosoUniversity/Global.asax.cs
+++ ContosoUniversity/Global.asax.cs
@@ -8,13 +8,19 @@ using System.Web.Routing;
 using ContosoUniversity.DAL;
 using System.Data.Entity.Infrastructure.Interception;
 
+
 namespace ContosoUniversity
 {
-    public class MvcApplication : System.Web.HttpApplication
-    {
-        protected void Application_Start()
-        {
-
-        }
-    }
+	public class MvcApplication : System.Web.HttpApplication
+	{
+		protected void Application_Start()
+		{
+			AreaRegistration.RegisterAllAreas();
+			FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+			RouteConfig.RegisterRoutes(RouteTable.Routes);
+			BundleConfig.RegisterBundles(BundleTable.Bundles);
+			DbInterception.Add(new SchoolInterceptorTransientErrors());
+			DbInterception.Add(new SchoolInterceptorLogging());
+		}
+	}
 }
